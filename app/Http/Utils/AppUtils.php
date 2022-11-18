16c2a8da422daaf97e1979edf4394205f8f7dbb9
doanh1105 <ngocanh1105@gmail.com>
2022-11-18 22:20:29 +0700

    seeder user datas

---
+++ app/Http/Utils/AppUtils.php
@@ -0,0 +1,8 @@
+<?php
+
+class AppUtils{
+    const ROLE_ADMIN = 0;
+    const ROLE_GIAO_VU = 1;
+    const ROLE_GIANG_VIEN = 2;
+    const ROLE_SINH_VIEN = 3;
+}
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,17 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
+        User::create([
+            'ho' => 'root',
+            'ten' => 'admin',
+            'username' => 'admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => AppUtils::ROLE_ADMIN,
+            'id_KhoaHoc' => null,
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
         factory(User::class,10)->create();
     }
 }
