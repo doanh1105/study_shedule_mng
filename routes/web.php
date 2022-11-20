055b5fa9cf554a0ef02fe0f4559aeb6e78bbdb84
doanh1105 <ngocanh1105@gmail.com>
2022-11-20 16:43:40 +0700

    user home page layout

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dang-nhap',[AuthController::class,'index'])->name('login.view');
 Route::post('/dang-nhap',[AuthController::class,'login'])->name('login.post');
 
+Route::get('/dang-xuat',[AuthController::class,'logout'])->name('logout');
 
 Route::middleware('auth.user')->name('user.')->group(function (){
     Route::get('/',[HomeController::class,'index'])->name('home');
