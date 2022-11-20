755f4f1219644c5ef79c6895069027ba8359d5ff
doanh1105 <ngocanh1105@gmail.com>
2022-11-20 16:46:35 +0700

    logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,19 +19,13 @@
     <link rel="stylesheet" href="{{asset('css/app-light.css')}}" id="lightTheme">
     <link rel="stylesheet" href="{{asset('css/app-dark.css')}}" id="darkTheme" disabled>
   </head>
-  <body class="light ">
+  <body class="light">
     <div class="wrapper vh-100">
       <div class="row align-items-center h-100">
         <form method="POST" action="{{route('login.post')}}" class="col-lg-3 col-md-4 col-10 mx-auto text-center">
             @csrf
           <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
-            <svg version="1.1" id="logo" class="navbar-brand-img brand-md" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120" xml:space="preserve">
-              <g>
-                <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
-                <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
-                <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
-              </g>
-            </svg>
+            <img class="logo" src="{{ asset('assets/images/logo.ico')}}" alt="">
           </a>
           <h1 class="h6 mb-3">Đăng nhập</h1>
           @if (Session::has('error'))
