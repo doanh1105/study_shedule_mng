007338b6029561e4b1004195e6184de13ffd0d7f
doanh1105 <ngocanh1105@gmail.com>
2022-12-21 17:44:34 +0700

    info: admin && giao vu dashboard

--- app/Http/Controllers/Auth/HomeController.php
+++ app/Http/Controllers/Auth/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Utils\AppUtils;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -20,7 +21,18 @@ class HomeController extends Controller
     }
 
     public function admin_index_view(){
-        return view('user.admin_gv.dashboard.dashboard');
+        $count_khoaHoc = DB::table('khoa_hocs')->get()->count();
+        $count_giangVien = DB::table('users')->where('role',AppUtils::ROLE_GIANG_VIEN)->get()->count();
+        $count_sinhVien = DB::table('users')->where('role',AppUtils::ROLE_SINH_VIEN)->get()->count();
+        $count_phongHoc = DB::table('phong_hocs')->get()->count();
+        $count_monHoc = DB::table('mon_hocs')->get()->count();
+        return view('user.admin_gv.dashboard.dashboard',[
+            'count_khoaHoc' => $count_khoaHoc,
+            'count_giangVien' => $count_giangVien,
+            'count_sinhVien' => $count_sinhVien,
+            'count_phongHoc' => $count_phongHoc,
+            'count_monHoc' => $count_monHoc
+        ]);
     }
 
     public function user_index_view(){
--- resources/views/user/admin_gv/dashboard/dashboard.blade.php
+++ resources/views/user/admin_gv/dashboard/dashboard.blade.php
@@ -16,9 +16,100 @@
       <div class="row justify-content-center">
         <div class="col-12">
           <div class="row align-items-center mb-2">
-            <div class="col">
-              <h2 class="h5 page-title">Dashboard</h2>
+            <div class="col-12 mb-5">
+              <h2 class="h5 page-title">Thống kê</h2>
             </div>
+              <div class="col-12">
+                  <div class="row">
+                      <div class="col-md-6 mb-4">
+                          <div class="card shadow bg-primary-darker text-white border-0">
+                              <div class="card-body">
+                                  <div class="row align-items-center">
+                                      <div class="col-3 text-center">
+                       <span class="circle circle-sm bg-primary">
+                            <i class="fe fe-16 fe-layers text-white mb-0"></i>
+                          </span>
+                                      </div>
+                                      <div class="col pr-0">
+                                          <p class="small text-white mb-0">Khoá đào tạo</p>
+                                          <span class="h3 mb-0 text-white">{{$count_khoaHoc}}</span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-4">
+                          <div class="card shadow bg-success text-black-50 border-0">
+                              <div class="card-body">
+                                  <div class="row align-items-center">
+                                      <div class="col-3 text-center">
+                          <span class="circle circle-sm bg-primary">
+                            <i class="fe fe-16 fe-user text-white mb-0"></i>
+                          </span>
+                                      </div>
+                                      <div class="col pr-0">
+                                          <p class="small text-white mb-0">Giảng viên</p>
+                                          <span class="h3 text-white mb-0">{{$count_giangVien}}</span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-4">
+                          <div class="card shadow bg-danger-light border-0">
+                              <div class="card-body">
+                                  <div class="row align-items-center">
+                                      <div class="col-3 text-center">
+                          <span class="circle circle-sm bg-primary">
+                            <i class="fe fe-16 fe-users text-white mb-0"></i>
+                          </span>
+                                      </div>
+                                      <div class="col pr-0">
+                                          <p class="small text-white mb-0">Sinh viên</p>
+                                          <span class="h3 text-white mb-0">{{$count_sinhVien}}</span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-md-6 mb-4">
+                          <div class="card shadow bg-info border-0">
+                              <div class="card-body">
+                                  <div class="row align-items-center">
+                                      <div class="col-3 text-center">
+                          <span class="circle circle-sm bg-primary">
+                            <i class="fe fe-16 fe-codepen text-white mb-0"></i>
+                          </span>
+                                      </div>
+                                      <div class="col pr-0">
+                                          <p class="small text-white mb-0">Phòng học</p>
+                                          <span class="h3 text-white mb-0">{{$count_phongHoc}}</span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+
+                      <div class="col-md-6 mb-4">
+                          <div class="card shadow bg-dark-lighter border-0">
+                              <div class="card-body">
+                                  <div class="row align-items-center">
+                                      <div class="col-3 text-center">
+                          <span class="circle circle-sm bg-primary">
+                            <i class="fe fe-16 fe-book-open text-white mb-0"></i>
+                          </span>
+                                      </div>
+                                      <div class="col pr-0">
+                                          <p class="small text-white mb-0">Môn học</p>
+                                          <span class="h3 text-white mb-0">{{$count_monHoc}}</span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+
+                  </div>
+              </div>
           </div>
         </div> <!-- .col-12 -->
       </div> <!-- .row -->
